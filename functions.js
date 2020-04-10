6644cbc66671ac09f6bd1ad53baee5dfe003caec
Mario <mariopirris@gmail.com>
2020-04-10 23:44:58 +0200

    fixes, table, data optimize

---
+++ functions.js
@@ -0,0 +1,23 @@
+function adjustNumber(number)
+{
+    if (number % 1 != 0)
+    {
+        number = number.toFixed(1);
+        console.log(number);
+        if (number % 1 == 0)
+            number = number.slice(0,-2);
+    }
+    return number;
+}
+
+function sortOptions() 
+{
+    var options = $("#citySelect option");                    // Collect options         
+    options.detach().sort(
+        function(a,b) {               // Detach from select, then Sort
+            var at = $(a).text();
+            var bt = $(b).text();         
+            return (at > bt)?1:((at < bt)?-1:0);            // Tell the sort function how to order
+    });
+    options.appendTo("#citySelect");   
+}
--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const cityListUrl = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province-latest.json";
 const cityState = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province.json";
 
+var stateCache = 0;
+
 $( document ).ready(function() {
-
-
-
-    console.log("partito")
     
     function init()
     {
@@ -13,62 +11,67 @@ $( document ).ready(function() {
             goTable($("#citySelect option:selected").text())
           });
 
-
-
         $.get(cityListUrl, function( data ) {
-    
-            
             $.each(JSON.parse(data), function(index, value){
                 if(value.denominazione_provincia !== "In fase di definizione/aggiornamento")
-                $('#citySelect').append(
-                    new Option(value.denominazione_provincia, value.denominazione_provincia));});
-          });
-    }
-
-    function goTable(city)
-    {
-
-        $.get(cityState, function( data ) {
-            aggregate = 0;
-            previousRow = 0;
-            percentage = 0
-            increment = 0
-            $("#tableBody").empty();
-
-            $.each(JSON.parse(data), function(index, value){
-                if(value.denominazione_provincia === city && value.totale_casi != 0)
-                {
-                    increment = value.totale_casi - previousRow;
-                    if (previousRow != 0)
-                        percentage = (100*increment) / previousRow;
-                    aggregate++;
-                    previousRow = value.totale_casi;
-                    $("#tableBody")
-                    .append($('<tr>')
-                    .append($('<th>').append(aggregate))
-                    .append($('<td>').append(city))
-                    .append($('<td>').append(value.data))
-                    .append($('<td>').append(value.totale_casi))
-                    .append($('<td>').append(increment))
-                    .append($('<td>').append(percentage.toFixed(2) + "%")));
-                }
-                
+                    $('#citySelect').append(
+                        new Option(value.denominazione_provincia, value.denominazione_provincia));
             });
+            sortOptions();
           });
     }
-    
-    
+
+    function getCityState()
+    {
+        if (stateCache != 0)
+            return new Promise(resolve =>  resolve(stateCache));
+
+        return $.get(cityState, function( data ) {
+            stateCache = data;
+            return stateCache;
+        });
+    }
+
+    async function goTable(city)
+    {
+        state = await getCityState(); 
+        aggregate = 0;
+        previousRow = 0;
+        percentage = 0
+        increment = 0
+        $("#tableBody").empty();
+        $(".table").show();
+
+        $.each(JSON.parse(state), function(index, value){
+            if(value.denominazione_provincia === city && value.totale_casi != 0)
+            {
+                increment = value.totale_casi - previousRow;
+                if (previousRow != 0)
+                    percentage = (100*increment) / previousRow;
+                aggregate++;
+                previousRow = value.totale_casi;
+                $("#tableBody")
+                .append($('<tr>')
+                .append($('<th>').append(aggregate))
+                .append($('<td>').append(city))
+                .append($('<td>').append(value.data))
+                .append($('<td>').append(value.totale_casi))
+                .append($('<td>').append(increment))
+                .append($('<td>').append(adjustNumber(percentage) + "%")));
+            }
+        });
+    }
+
+
     
     init();
     });
 
 
-$("#tableID").find('tbody')
-    .append($('<tr>')
-        .append($('<td>')
-            .append($('<img>')
-                .attr('src', 'img.png')
-                .text('Image cell')
-            )
-        )
-    );
+    /* TODO
+        Dati totali Italia
+        Dati totali provincia
+        Dati totali regione
+        Tabella a comparsa
+        Grafici!
+    */
