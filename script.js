2c80712b743ac232364d10beebd01ed3bfb2143f
Mario <mariopirris@gmail.com>
2020-04-12 15:06:11 +0200

    graphics improve

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const cityListUrl = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province-latest.json";
 const cityState = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province.json";
+const regionState = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni-latest.json";
+
 
 var stateCache = 0;
+var regionCache = 0;
 
 $( document ).ready(function() {
     
@@ -32,19 +35,35 @@ $( document ).ready(function() {
         });
     }
 
+    function getRegionState()
+    {
+        if (regionCache != 0)
+            return new Promise(resolve =>  resolve(regionCache));
+
+        return $.get(regionState, function( data ) {
+            regionCache = data;
+            return regionCache;
+        });
+    }
+
     async function goTable(city)
     {
         state = await getCityState(); 
+        regionsState = await getRegionState();
         aggregate = 0;
         previousRow = 0;
         percentage = 0
         increment = 0
         $("#tableBody").empty();
         $(".table").show();
+        $("#provincia").text(city);
+        region = undefined;
+        console.log(regionsState);
 
         $.each(JSON.parse(state), function(index, value){
             if(value.denominazione_provincia === city && value.totale_casi != 0)
             {
+                region = value.codice_regione;
                 increment = value.totale_casi - previousRow;
                 if (previousRow != 0)
                     percentage = (100*increment) / previousRow;
@@ -52,14 +71,24 @@ $( document ).ready(function() {
                 previousRow = value.totale_casi;
                 $("#tableBody")
                 .append($('<tr>')
-                .append($('<th>').append(aggregate))
-                .append($('<td>').append(city))
+                .append($('<td>').append(aggregate))
                 .append($('<td>').append(value.data.slice(0, -9)))
                 .append($('<td>').append(value.totale_casi))
                 .append($('<td>').append(increment))
                 .append($('<td>').append(adjustNumber(percentage) + "%")));
             }
         });
+        $.each(JSON.parse(regionCache), function(index, value){
+            if (value.codice_regione === region)
+            {
+                console.log(value);
+                $("#contagi").text(value.totale_casi);
+                $("#guariti").text(value.dimessi_guariti);
+                $("#decessi").text(value.deceduti);
+                $("#tamponi").text(value.tamponi);
+                $("#regione").text(value.denominazione_regione);
+            }
+        })
     }
 
 
@@ -70,8 +99,6 @@ $( document ).ready(function() {
 
     /* TODO
         Dati totali Italia
-        Dati totali provincia
-        Dati totali regione
         Tabella a comparsa
         Grafici!
     */
